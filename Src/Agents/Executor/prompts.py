47ac453df69009f3489f365e88ab7409404fee69
= <nihar.shettigar@gmail.com>
2025-06-14 21:31:41 +0530

    minor prompt improvement

--- Src/Agents/Executor/prompts.py
+++ Src/Agents/Executor/prompts.py
@@ -42,15 +42,18 @@ These are the things that you learned from the mistakes you made earlier :
   - This is a standard Python environment, not a python notebook or a repl. previous execution
     context is not preserved between executions.
   - Don't execute dangerous commands like rm -rf * or access sensitive files
-  - If you are stuck, have tried to fix an issue (e.g., a linter error) multiple times (e.g., 3 times) without success, or need clarification, ask the USER for input. Explain the situation clearly.
+  - If you are stuck, have tried to fix an issue (e.g., a linter error) multiple times (e.g., 3 times) without success, or need clarification, ask the USER for input.
   - Upon creating anything (like a new project, website, data analysis png) always show the output.You can do this by executing shell commands.
   - the python/shell code execution through tool call will be executed immediately and output will be shown. it wont be saved.
+  - When asked to do research, use the web_search and web_loader tools to do in depth research. Use multiple iterations and get information from multiple sources. Analyse data and provide insights.
 
 
 ** Important **
 - You can only perform one tool call at a time.
 - Always evaluate the output of the tool call before deciding the next step.
-- Continue performing actions until the user's goal is fully achieved. Only then, include 'TASK_DONE' in your response if that is the required signal for completion.
+- Continue performing actions until the user's goal is fully achieved. Only then, include 'TASK_DONE' in your response.
 - Do not end the task immediately after a tool call without evaluating its output.
+- The best way to give output is to save it open the file using shell commands.
+
 
 """
