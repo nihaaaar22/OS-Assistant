7ebc7234f01a7580d84c8eac78053cd02e82e3a1
= <nihar.shettigar@gmail.com>
2025-06-22 13:06:56 +0530

    minor import changes of deleted modules

--- Src/Env/__init__.py
+++ Src/Env/__init__.py
@@ -1,5 +1,1 @@
-from .base_executor import BaseExecutor
-from .python_executor import PythonExecutor
-from .js_executor import JavaScriptExecutor
 
-__all__ = ["BaseExecutor", "PythonExecutor", "JavaScriptExecutor"]
--- Src/Env/python_executor.py
+++ Src/Env/python_executor.py
@@ -1,20 +1,3 @@
-# from .base_executor import BaseExecutor
-
-# class PythonExecutor():
-#     def execute(self, code: str) -> str:
-#         """Executes Python code and returns the result or an error message."""
-
-#         # if not self.validate_code(code):
-#         #     return "Code validation failed: Unsafe code detected."
-
-#         local_vars = {}
-#         try:
-#             exec(code, {}, local_vars)  # Execute code in an isolated environment
-#             return local_vars.get("output", "Code executed successfully.")
-#         except Exception as e:
-#             # return self.handle_error(e)
-#             print("error in running python code", e)
-
 import subprocess
 import tempfile
 import os
